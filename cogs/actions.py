d73fdbc2acbc66d699df7c2111c7d92f7f5ff06d
Atriays <aryan1024sethi@gmail.com>
2021-01-16 19:13:54 +0530

    cool

--- cogs/actions.py
+++ cogs/actions.py
@@ -14,50 +14,43 @@ class actions(commands.Cog):
 
 
 
+    def slaplol(self,pfp_url : str):
+        batman = 'https://cdn.discordapp.com/attachments/792026292781973515/793810852700094475/Batman-Slapping-Robin-Meme-Explained.png'
+        batman_opened = Image.open(BytesIO(requests.get(batman).content))
+        pfp_opened = Image.open(BytesIO(requests.get(pfp_url).content))
+
+        #resizing the pfp
+        pfp_opened = pfp_opened.resize((320,320))
+
+        #pasting the pfp on the slap bg
+        batman_opened.paste(pfp_opened, (290,215))
+        batman_opened = batman_opened.resize((round(batman_opened.size[0]*0.5), round(batman_opened.size[1]*0.5)))
+
+        with BytesIO() as image_binary:
+            batman_opened.save(image_binary, "PNG")
+            image_binary.seek(0)
+            #batman_opened.save('slap.png')
+            return discord.File(fp=image_binary,filename="slapppppp.png")
+
 
     @commands.command()
     async def slap(self, ctx, user : discord.Member = None):
-        batman = 'https://cdn.discordapp.com/attachments/792026292781973515/793810852700094475/Batman-Slapping-Robin-Meme-Explained.png'
         if user == None :
             user = ctx.author
 
             userpfp = user.avatar_url
             #img = Image.open(urlopen(url))
             #opening the imgs in pillow
-            batman_opened = Image.open(BytesIO(requests.get(batman).content))
-            pfp_opened = Image.open(BytesIO(requests.get(userpfp).content))
 
-            #resizing the pfp
-            pfp_opened = pfp_opened.resize((320,320))
+            file = self.slaplol(userpfp)
+            await ctx.send(file= file)
 
-            #pasting the pfp on the slap bg
-            batman_opened.paste(pfp_opened, (290,215))
-            batman_opened = batman_opened.resize((round(batman_opened.size[0]*0.5), round(batman_opened.size[1]*0.5)))
-
-            with BytesIO() as image_binary:
-                batman_opened.save(image_binary, "PNG")
-                image_binary.seek(0)
-                #batman_opened.save('slap.png')
-                await ctx.send(file=discord.File(fp=image_binary,filename="slapppppp.png"))
         else:
             userpfp = user.avatar_url
             #img = Image.open(urlopen(url))
             #opening the imgs in pillow
-            batman_opened = Image.open(BytesIO(requests.get(batman).content))
-            pfp_opened = Image.open(BytesIO(requests.get(userpfp).content))
-
-            #resizing the pfp
-            pfp_opened = pfp_opened.resize((320,320))
-
-            #pasting the pfp on the slap bg
-            batman_opened.paste(pfp_opened, (290,215))
-            batman_opened = batman_opened.resize((round(batman_opened.size[0]*0.5), round(batman_opened.size[1]*0.5)))
-
-            with BytesIO() as image_binary:
-                batman_opened.save(image_binary, "PNG")
-                image_binary.seek(0)
-                #batman_opened.save('slap.png')
-                await ctx.send(file=discord.File(fp=image_binary,filename="slapppppp.png"))
+            file = self.slaplol(userpfp)
+            await ctx.send(file= file)
 
 
 def setup(client):
